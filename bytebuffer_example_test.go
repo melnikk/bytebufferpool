d73f19e564a5445dfbeada0202f775446b0cdd99
Kirill Melnikov <melnikov@tochka.com>
2021-08-03 14:02:20 +0500

    go.mod fixed

--- bytebuffer_example_test.go
+++ bytebuffer_example_test.go
@@ -3,7 +3,7 @@ package bytebufferpool_test
 import (
 	"fmt"
 
-	"github.com/valyala/bytebufferpool"
+	"github.com/melnikk/bytebufferpool"
 )
 
 func ExampleByteBuffer() {
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/valyala/bytebufferpool
+module github.com/melnikk/bytebufferpool
 
-go 1.12
+go 1.16
